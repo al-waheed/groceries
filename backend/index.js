235648683b8cff1b1ae39bc50fefc4b903fbbd4e
morenikeji ajisegiri <ajisegirimorenikeji@gmail.com>
2023-10-23 10:28:26 +0100

    new-update

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.get("/", (req, res) => {
   res.send("Server working successfully");
 });
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
